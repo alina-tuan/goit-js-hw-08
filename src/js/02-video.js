50fdbee00588c566ea66f23c564dc1838fe90816
Alina <alina.bilchich@gmail.com>
2022-10-25 18:45:53 +0300

    task 2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+// час відтворення оновлюється у сховищі не частіше, ніж раз на секунду
+// зберігається час відтворення у локальне сховище - ключ для сховища "videoplayer-current-time"
+const onPlay = throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000);
+
+// відстеження події timeupdate - оновлення часу відтворення
+player.on("timeupdate", onPlay);
+
+
+// відновлення відтворення зі збереженої позиції
+player.on("loaded", function () {
+        if (localStorage.getItem("videoplayer-current-time")) {
+            player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+        }
+    });
